ada76301e62e9777657bd6b3fa55f070d4b8874c
PhilLeeChin <phil.leechin@outlook.com>
2021-03-14 14:39:13 -0400

    add association for commented_games

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :games
     has_many :comments
-    has_many :commented_games, through: :comments, source: :games
+    has_many :commented_games, through: :comments, source: :game
     has_many :genres, through: :games
 end
