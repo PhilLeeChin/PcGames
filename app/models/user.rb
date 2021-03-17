6cd3b3921ec820378a9cd2552f6ad43fd2b00f5b
PhilLeeChin <phil.leechin@outlook.com>
2021-03-16 20:09:08 -0400

    add method to link to omni model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
     has_many :comments
     has_many :commented_games, through: :comments, source: :game
     has_many :genres, through: :games
+
+    validates_presence_of :username, :email, :password
+    validates_uniqueness_of :username, :email
+
+    def self.find_with_omni(auth)
+        binding.pry
+    end
 end
