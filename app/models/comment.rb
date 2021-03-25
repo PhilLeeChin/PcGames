cf11ae3111cc58b7085fb9252cdeea2269386801
PhilLeeChin <phil.leechin@outlook.com>
2021-03-24 23:45:52 -0400

    add validates for content, user_id, game_id

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :game_id, presence: true
 end
