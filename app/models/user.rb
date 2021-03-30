b3cc7649a9b5b693f1f1ae34058e2fb1082f3b65
PhilLeeChin <phil.leechin@outlook.com>
2021-03-30 14:06:42 -0400

    add has_many resourzes, and omni method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 class User < ApplicationRecord
     has_many :games
     has_many :comments
@@ -5,10 +6,16 @@ class User < ApplicationRecord
     has_many :genres, through: :games
 
     has_secure_password
+    has_many :resourzes, through: :comments
     validates_presence_of :username, :email, :password
     validates_uniqueness_of :username, :email
 
     def self.find_with_omni(auth)
-        binding.pry
+        # binding.pry
+        self.find_or_create_by(uid: auth[:uid]) do |u|
+            u.username = auth[:info][:name]
+            u.email = auth[:info][:email]
+            u.password = SecureRandom.hex(12)
+        end
     end
 end
