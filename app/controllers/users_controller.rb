7bb70e4e8aed150701b8fd17addec02a2c23328e
PhilLeeChin <phil.leechin@outlook.com>
2021-03-30 14:05:54 -0400

    add before_action and show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,10 @@
 class UsersController < ApplicationController
-   # before_action :redirect_if_not_logged_in
-
+   before_action :redirect_if_not_logged_in
+   
    def show
-      # @user = User.find_by_id(params[:id])
-      # redirect_to '/' if !@user
-   end
-
-   def new
-      @user = User.new
-   end
-
-   def create
-      @user = User.new(user_params)
-      if @user.save
-         session[:user_id] = @user.id
-         redirect_to
-      else
-         render :new
-      end
-   end
-
-   private
-   def user_params
-      params.require(:user).permit(:username, :email, :password)
-   end
+      @user = User.find_by_id(params[:id])
+      redirect_to '/' if !@user
+      #binding.pry
+  end
+  
 end
